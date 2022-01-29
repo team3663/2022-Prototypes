37124b2da50274f056a964f28b31a8c11a817998
CamdonCon <52018350+CamdonCon@users.noreply.github.com>
2022-01-29 15:44:09 -0800

    changed constants

--- Prototypes/src/main/java/frc/robot/Constants.java
+++ Prototypes/src/main/java/frc/robot/Constants.java
@@ -16,6 +16,11 @@ public final class Constants {
     public static final int TEST_CONTROLLER_ID = 2;
 
     // CAN IDs for our motor controllers
+<<<<<<< Updated upstream
     public static final int MOTOR_A_CAN_ID = 1;
     public static final int MOTOR_B_CAN_ID = 2;
+=======
+    public static final int SHOOTER_1_CAN_ID = 2;
+    public static final int SHOOTER_2_CAN_ID = 3;
+>>>>>>> Stashed changes
 }
